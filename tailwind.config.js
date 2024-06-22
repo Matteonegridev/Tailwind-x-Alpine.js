f2b6d9e56cab5e1b64d07e24979cbdedf411e0de
Matteo <matteonegridev@gmail.com>
2024-06-22 10:39:02 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html", "./js/*.js"],
+  theme: {
+    extend: {
+      screens: {
+        xsm: { max: "639px" },
+
+        md: { min: "640px" },
+
+        lg: { min: "768px", max: "1023px" },
+
+        xl: { min: "1024px", max: "1279px" },
+
+        "2xl": { min: "1280px", max: "1535px" },
+
+        "3xl": { min: "1536px" },
+      },
+      transformOrigin: {
+        0: "0%",
+        35: "35px",
+        50: "50%",
+        100: "100%",
+      },
+    },
+    keyframes: {
+      "open-menu": {
+        "0%": { transform: "scaleY(0)" },
+        "80%": { transform: "scaleY(1.2)" },
+        "100%": { transform: "scaleY(1)" },
+      },
+    },
+    animation: {
+      "open-menu": "open-menu 0.5s ease-in-out forwards",
+    },
+  },
+  plugins: [],
+};
